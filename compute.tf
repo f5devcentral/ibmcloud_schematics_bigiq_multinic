f0174ff25d0df0f4e12eb1126b9a7dbabe8f7e99
John Gruber <j.gruber@f5.com>
2021-10-15 11:57:29 -0500

    updated BIG-IQ for license playbooks

--- compute.tf
+++ compute.tf
@@ -34,16 +34,6 @@ locals {
   license_basekey        = var.license_basekey == "none" ? "null" : var.license_basekey
   license_pool_name      = var.license_pool_name == "none" ? "null" : var.license_pool_name
   license_utility_regkey = var.license_utility_regkey == "none" ? "null" : var.license_utility_regkey
-  license_offerings_1    = var.license_offerings_1 == "none" ? "null" : var.license_offerings_1
-  license_offerings_2    = var.license_offerings_2 == "none" ? "null" : var.license_offerings_2
-  license_offerings_3    = var.license_offerings_3 == "none" ? "null" : var.license_offerings_3
-  license_offerings_4    = var.license_offerings_4 == "none" ? "null" : var.license_offerings_4
-  license_offerings_5    = var.license_offerings_5 == "none" ? "null" : var.license_offerings_5
-  license_offerings_6    = var.license_offerings_6 == "none" ? "null" : var.license_offerings_6
-  license_offerings_7    = var.license_offerings_7 == "none" ? "null" : var.license_offerings_7
-  license_offerings_8    = var.license_offerings_8 == "none" ? "null" : var.license_offerings_8
-  license_offerings_9    = var.license_offerings_9 == "none" ? "null" : var.license_offerings_9
-  license_offerings_10   = var.license_offerings_10 == "none" ? "null" : var.license_offerings_10
 }
 
 data "template_file" "user_data" {
@@ -53,16 +43,7 @@ data "template_file" "user_data" {
     license_basekey        = local.license_basekey
     license_pool_name      = local.license_pool_name
     license_utility_regkey = local.license_utility_regkey
-    license_offerings_1    = local.license_offerings_1
-    license_offerings_2    = local.license_offerings_2
-    license_offerings_3    = local.license_offerings_3
-    license_offerings_4    = local.license_offerings_4
-    license_offerings_5    = local.license_offerings_5
-    license_offerings_6    = local.license_offerings_6
-    license_offerings_7    = local.license_offerings_7
-    license_offerings_8    = local.license_offerings_8
-    license_offerings_9    = local.license_offerings_9
-    license_offerings_10   = local.license_offerings_10
+    license_regkeys        = join(", ", var.license_regkey_offerings)
     phone_home_url         = local.phone_home_url
     template_source        = var.template_source
     template_version       = var.template_version
@@ -73,7 +54,7 @@ data "template_file" "user_data" {
 }
 
 # create compute instance
-resource "ibm_is_instance" "f5_ve_instance" {
+resource "ibm_is_instance" "f5_bigiq_instance" {
   name    = var.instance_name
   image   = local.image_id
   profile = data.ibm_is_instance_profile.instance_profile.id
@@ -101,44 +82,5 @@ resource "ibm_is_instance" "f5_ve_instance" {
 # create floating IP for management access
 resource "ibm_is_floating_ip" "f5_management_floating_ip" {
   name   = "f0-${random_uuid.namer.result}"
-  target = ibm_is_instance.f5_ve_instance.primary_network_interface.0.id
-}
-
-# create 1:1 floating IPs to vNICs - Not supported by IBM yet
-#resource "ibm_is_floating_ip" "f5_data_floating_ips" {
-#  count = length(local.secondary_subnets)
-#  name   = format("f%d-%s", (count.index+1), random_uuid.namer.result)
-#  target = ibm_is_instance.f5_ve_instance.network_interfaces[count.index].id
-#}
-
-output "resource_name" {
-  value = ibm_is_instance.f5_ve_instance.name
-}
-
-output "resource_status" {
-  value = ibm_is_instance.f5_ve_instance.status
-}
-
-output "VPC" {
-  value = ibm_is_instance.f5_ve_instance.vpc
-}
-
-output "image_id" {
-  value = data.ibm_is_image.bigiq_custom_image.id
-}
-
-output "instance_id" {
-  value = ibm_is_instance.f5_ve_instance.id
-}
-
-output "profile_id" {
-  value = data.ibm_is_instance_profile.instance_profile.id
-}
-
-output "f5_shell_access" {
-  value = "ssh://root@${ibm_is_floating_ip.f5_management_floating_ip.address}"
-}
-
-output "f5_phone_home_url" {
-  value = var.phone_home_url
+  target = ibm_is_instance.f5_bigiq_instance.primary_network_interface.0.id
 }
